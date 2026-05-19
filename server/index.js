6a2bbcee5b5c9d98beba6554056f119abb0c1401
piyadaete-ops <piyadaeye@gmail.com>
2026-05-19 22:29:26 +0700

    fix: แก้ port

--- server/index.js
+++ server/index.js
@@ -60,6 +60,6 @@ app.put("/todos/:id", async (req, res) => {
 })
 // app.listen(3000, () => console.log("Server running"))
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server is running on port ${PORT}`);
-});   
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
