bcfabbe7b6fa6eb4b277abeb34be7abbbc9e63e0
piyadaete-ops <piyadaeye@gmail.com>
2026-05-19 22:06:55 +0700

    fix: loginpage.css

--- fh-rep/src/pages/logInPage.jsx
+++ fh-rep/src/pages/logInPage.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import '../styles/Loginpage.css';
+import '../styles/LoginPage.css';
 
 function LoginPage({ onLoginSuccess,goToRegister }) {
   const [username, setUsername] = useState('');
